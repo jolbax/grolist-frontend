4b7765e5c91ba2307c7a4d08b9900a213a3f4056
Jose Luis Barahona <jolubax@icloud.com>
2019-06-20 10:57:54 +0200

    Configuration for firebase hosting deployment

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,32 +1,36 @@
 import axios from "axios";
 
+const API_ENDPOINT = process.env.REACT_APP_GROLIST_API_URL;
+
 const api = {
   fetchLists() {
-    return axios.get("/api/lists/all");
+    return axios.get(`${API_ENDPOINT}/api/lists/all`);
   },
   createList(list) {
-    return axios.post(`/api/lists/create`, list);
+    return axios.post(`${API_ENDPOINT}/api/lists/create`, list);
   },
   updateList(id, updatedList) {
-    return axios.post(
-      `/api/lists/${id}/update`,
-      updatedList
-    );
+    return axios.post(`${API_ENDPOINT}/api/lists/${id}/update`, updatedList);
   },
   deleteList(id) {
-    return axios.delete(`/api/lists/${id}/delete`);
+    return axios.delete(`${API_ENDPOINT}/api/lists/${id}/delete`);
   },
   createItem(item) {
-    return axios.post(`/api/lists/${item.listId}/items/create`, item);
+    return axios.post(
+      `${API_ENDPOINT}/api/lists/${item.listId}/items/create`,
+      item
+    );
   },
   updateItem(item, updatedItem) {
     return axios.post(
-      `/api/lists/${item.listId}/items/${item.id}/update`,
+      `${API_ENDPOINT}/api/lists/${item.listId}/items/${item.id}/update`,
       updatedItem
     );
   },
   deleteItem(item) {
-    return axios.delete(`/api/lists/${item.listId}/items/${item.id}/delete`);
+    return axios.delete(
+      `${API_ENDPOINT}/api/lists/${item.listId}/items/${item.id}/delete`
+    );
   }
 };
 
